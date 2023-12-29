bbfb8f0f37bc0c63855544d5a1604cb77daceb1f
siddeshg907 <121315414+siddeshg907@users.noreply.github.com>
2023-12-30 03:34:35 +0530

    created users profile signout,update backend and functionality,UI

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -19,7 +19,7 @@ export default function SignIn() {
   const handleSubmit=async(e)=>{
     e.preventDefault()
     try {
-      setLoading(true)
+      dispatch(signInStart())
     const res=await fetch('/api/auth/signin',{
       method:'POST',
       headers:{
