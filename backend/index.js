034c0573d83d6c0466495c6e6ca71233a449224c
siddeshg907 <121315414+siddeshg907@users.noreply.github.com>
2023-12-30 17:01:12 +0530

    added api route for creating new list

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv, { config } from "dotenv";
 import authRouter from "./routes/auth.route.js";
 import userRouter from "./routes/user.route.js";
+import listingRouter from "./routes/listing.route.js";
 import cookieParser from "cookie-parser";
 
 dotenv.config()
@@ -27,6 +28,7 @@ app.get("/",(req,res)=>{
 
 app.use("/api/user",userRouter)
 app.use("/api/auth",authRouter)
+app.use("/api/listing",listingRouter)
 
 app.use((err,req,res,next)=>{
    const statusCode=err.statusCode||500
