7efea143caf0f39f4edd30b855cc93ad6cb973d9
siddeshg907 <121315414+siddeshg907@users.noreply.github.com>
2024-01-01 21:27:14 +0530

    created api Route,Delete ,Edit functionality and Page,Single Property page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,8 @@ import Profile from "./pages/Profile";
 import Navbar from "./components/Navbar.jsx";
 import PrivateRoute from "./components/PrivateRoute.jsx";
 import CreateListing from "./pages/CreateListing.jsx";
+import UpdateListing from "./pages/UpdateListing.jsx";
+import Listing from "./pages/Listing.jsx";
 
 export default function App() {
   return (
@@ -17,9 +19,11 @@ export default function App() {
       <Route path="/sign-in" element={<SignIn/>}/>
       <Route path="/sign-up" element={<SignUp/>}/>
       <Route path="/about" element={<About/>}/>
+      <Route path="/listing/:listingId" element={<Listing/>} />
       <Route element={<PrivateRoute/>}>
       <Route path="/profile" element={<Profile/>}/>
       <Route path="/create-listing" element={<CreateListing/>}/>
+      <Route path="/update-listing/:listingId" element={<UpdateListing/>}/>
       </Route>
     </Routes>
     </BrowserRouter>
